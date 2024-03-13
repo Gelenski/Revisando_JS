e8ea629d3137c0219528e7321c91576aa2143d5e
Lucas Gelenski <gelenskilucas@gmail.com>
2024-03-13 12:32:40 -0300

    Adicionando os arquivos iniciais

---
+++ tipo-numero.js
@@ -0,0 +1,14 @@
+//* Números inteiros:
+const primeiroNumero = 10;
+const segundoNumero = 20;
+console.log(primeiroNumero + segundoNumero);
+
+//* Ponto Flutuante
+const primeiroDecimal = 3.14;
+const segundoDecimal = 0.5;
+console.log(primeiroDecimal / segundoDecimal);
+console.log(primeiroDecimal - segundoDecimal);
+console.log(primeiroDecimal * segundoDecimal);
+
+const divisao = primeiroDecimal / segundoDecimal;
+console.log(divisao.toFixed(1));
---
+++ tipo-texto.js
@@ -0,0 +1,6 @@
+const nomeAluno = "Beatriz";
+const maeAluno = "Juliana";
+console.log(nomeAluno);
+const idadeAluno = 17;
+console.log(nomeAluno * idadeAluno);
+console.log(nomeAluno + " " + maeAluno); //* Concatenação
